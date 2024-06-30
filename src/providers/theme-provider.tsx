e607c44b54681c5c916a9e77ea8ffff38af96f95
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-06-30 13:38:08 +0000

    Antd and theme setup

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { Children } from "react";
+import { ConfigProvider } from "antd";
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#1B4242",
+            // borderRadius: 2,
+          },
+          components: {
+            Button: {
+              controlHeight: 40,
+              boxShadow: "none",
+              colorPrimaryActive: "#1B4242",
+              //   controlOutline: "none",
+              colorBorder: "#1B4242",
+            },
+            Input: {
+              controlHeight: 45,
+              boxShadow: "none",
+              activeShadow: "#1B4242",
+            },
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </div>
+  );
+}
+
+export default ThemeProvider;
