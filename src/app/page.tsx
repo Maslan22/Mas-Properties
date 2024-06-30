cd85be2a94f303c2c391a851758c60b224fa1965
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-06-30 17:29:47 +0000

    auth setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import { Button, Flex, Input } from 'antd';
-import { UserButton, useUser } from '@clerk/nextjs';
+"use client";
+import { Button, Flex, Input } from "antd";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
-  const loggedInUser = useUser() as any;
-  console.log(loggedInUser);
+  const { user } = useUser();
+
+  let username = user?.username;
+  if (!username) {
+    username = user?.firstName + " " + user?.lastName;
+  }
+
+  username = username.replace("null", "");
   return (
-    <div className="p-20 flex items-center justify-center flex-col gap-10 h-screen">
-      <UserButton />
+    <div className="flex items-center justify-center flex-col gap-10 h-screen">
+      <UserButton afterSignOutUrl="/sign-in" signInUrl="/sign-in"/>
+      <h1>Clerk user id: {user?.id}</h1>
+      <h1>Username: {username}</h1>
+      <h1>Email: {user?.emailAddresses[0].emailAddress}</h1>
     </div>
   );
 }
