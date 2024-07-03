4c87eab33899cfd1bf59a628047e6b427a671cb7
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-07-03 01:40:22 +0000

    Location step done

--- src/app/(private)/user/properties/_componets/properties-form/location.tsx
+++ src/app/(private)/user/properties/_componets/properties-form/location.tsx
@@ -1,23 +1,62 @@
 import React from "react";
 import { PropertiesFormStepProps } from ".";
-import { Button } from "antd";
+import { Button, Form, Input, InputNumber, Select } from "antd";
+import { cities } from "@/constants";
 
-function Location({ currentStep, setCurrentStep }: PropertiesFormStepProps) {
+function Location({
+  currentStep,
+  setCurrentStep,
+  setFinalValues,
+  finalValues,
+}: PropertiesFormStepProps) {
+  const onFinish = (values: any) => {
+    setFinalValues({ ...finalValues, location: values });
+    setCurrentStep(currentStep + 1);
+  };
+  //city, pincode, landmark, address
   return (
-    <div>
-      <span>Location</span>
-      <div className="flex justify-end gap-5">
+    <Form layout="vertical" onFinish={onFinish}>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <Form.Item
+          name="city"
+          label="City"
+          rules={[{ required: true, message: "Please input City" }]}
+        >
+          <Select options={cities} placeholder="City" />
+        </Form.Item>
+        <Form.Item
+          name="pincode"
+          label="Pincode"
+          rules={[{ required: true, message: "Please input Pincode" }]}
+        >
+          <InputNumber placeholder="Pincode"  className="w-full"/>
+        </Form.Item>
+        <Form.Item
+          name="landmark"
+          label="Landmark"
+          rules={[{ required: true, message: "Please input Landmark" }]}
+        >
+          <Input placeholder="Landmark" />
+        </Form.Item>
+        <Form.Item
+          name="address"
+          label="Address"
+          rules={[{ required: true, message: "Please input Address" }]}
+          className="col-span-1 lg:col-span-3"
+        >
+          <Input.TextArea rows={5} placeholder="Address" />
+        </Form.Item>
+      </div>
+      <div className="flex justify-end gap-5 mt-7">
         <Button
           disabled={currentStep === 0}
           onClick={() => setCurrentStep(currentStep - 1)}
         >
           Back
         </Button>
-        <Button type="primary" onClick={() => setCurrentStep(currentStep + 1)}>
-          Next
-        </Button>
+        <Button htmlType="submit" type="primary">Next</Button>
       </div>
-    </div>
+    </Form>
   );
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,3 +11,11 @@ export const propertyStatus = [
   { value: "rent", label: "Rent" },
   { value: "sale", label: "Sale" },
 ];
+
+export const cities = [
+  {value: "accra", label: "Accra"},
+  {value: "kumasi", label: "Kumasi"},
+  {value: "cape coast", label: "Cape Coast"},
+  {value: "tamale", label: "Tamale"},
+  {value: "ho", label: "Ho"},
+];
