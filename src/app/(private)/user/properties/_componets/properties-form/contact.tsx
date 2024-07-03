d06b00ff6f27d77dfc9bac5002677d2829bcad8a
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-07-03 22:59:25 +0000

    Contact setup donde

--- src/app/(private)/user/properties/_componets/properties-form/contact.tsx
+++ src/app/(private)/user/properties/_componets/properties-form/contact.tsx
@@ -1,23 +1,74 @@
 import React from "react";
 import { PropertiesFormStepProps } from ".";
-import { Button } from "antd";
+import { Button, Form, Input, InputNumber, Select } from "antd";
 
-function Contact({ currentStep, setCurrentStep }: PropertiesFormStepProps) {
+function Contact({
+  currentStep,
+  setCurrentStep,
+  finalValues,
+  setFinalValues,
+}: PropertiesFormStepProps) {
+  const onFinish = (values: any) => {
+    const tempFinalValues = { ...finalValues, contact: values };
+    console.log(tempFinalValues);
+  };
+  //ownerName, ownerEmail, ownerNumber, ownerAddress, showOwnerDetails
   return (
-    <div>
-      <span>Contact</span>
-      <div className="flex justify-end gap-5">
+    <Form
+      layout="vertical"
+      onFinish={onFinish}
+      initialValues={finalValues.contact}
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <Form.Item
+          name="ownerName"
+          label="Owner Name"
+          rules={[{ required: true, message: "Please input Owner Name" }]}
+        >
+          <Input placeholder="Owner Name" />
+        </Form.Item>
+        <Form.Item
+          name="ownerEmail"
+          label="Owner Email"
+          rules={[{ required: true, message: "Please input Owner Email" }]}
+        >
+          <Input placeholder="Owner Email" />
+        </Form.Item>
+        <Form.Item
+          name="ownerNumber"
+          label="Owner Number"
+          rules={[{ required: true, message: "Please input Owner Number" }]}
+        >
+          <Input placeholder="Owner Number" />
+        </Form.Item>
+        <Form.Item
+          name="showOwnerContact"
+          label="Show Owner Contact"
+          rules={[
+            { required: true, message: "Please input Show Owner Contact" },
+          ]}
+        >
+          <Select
+            placeholder="Owner Address"
+            options={[
+              { label: "Yes", value: "yes" },
+              { label: "No", value: "no" },
+            ]}
+          />
+        </Form.Item>
+      </div>
+      <div className="flex justify-end gap-5 mt-7">
         <Button
           disabled={currentStep === 0}
           onClick={() => setCurrentStep(currentStep - 1)}
         >
           Back
         </Button>
-        <Button type="primary" onClick={() => setCurrentStep(currentStep + 1)}>
+        <Button type="primary" htmlType="submit">
           Next
         </Button>
       </div>
-    </div>
+    </Form>
   );
 }
 
