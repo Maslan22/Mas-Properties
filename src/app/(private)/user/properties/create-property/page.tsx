1b04643b91b2dcbc90822731acb4326188e09eb5
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-07-05 23:12:38 +0000

    Clone properties

--- src/app/(private)/user/properties/create-property/page.tsx
+++ src/app/(private)/user/properties/create-property/page.tsx
@@ -1,12 +1,23 @@
 import PageTitle from "@/componets/page-title";
 import React from "react";
 import PropertiesForm from "../_componets/properties-form";
+import prisma from "@/config/db";
+import { Property } from "@prisma/client";
 
-function CreatePropertyPage() {
+async function CreatePropertyPage({ searchParams }: { searchParams: any }) {
+  const cloneFrom = searchParams?.cloneFrom || "";
+  let property: Property | null = null;
+  if (cloneFrom) {
+    property = (await prisma.property.findUnique({
+      where: {
+        id: cloneFrom,
+      },
+    })) as Property;
+  }
   return (
     <div>
       <PageTitle title="Create Property" />
-      <PropertiesForm />
+      <PropertiesForm initialValues={property ? property : {}} />
     </div>
   );
 }
